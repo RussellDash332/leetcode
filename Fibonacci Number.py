eec71380a7847de1623fcd4ab2982d82a1973b13
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2021-10-07 22:12:50 +0800

    Create Fibonacci Number.py

---
+++ Fibonacci Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+            
+        return a
