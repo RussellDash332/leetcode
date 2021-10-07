fa9008cb75a8b524a788f101c2a8c8c9f9a96e83
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2021-10-07 22:13:19 +0800

    Create N-th Tribonacci Number.py

---
+++ N-th Tribonacci Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b, c = 0, 1, 1
+        for _ in range(n):
+            a, b, c = b, c, a + b + c
+            
+        return a
