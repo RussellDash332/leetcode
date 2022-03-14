277c79019ed675e5b78b73db039d273f0d37d7c6
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2022-03-15 02:41:46 +0800

    Update Single Number III.py

--- Single Number III.py
+++ Single Number III.py
@@ -5,4 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         # Not optimal but works
-        return list(filter(lambda x: nums.count(x) != 2, nums))
+        # return list(filter(lambda x: nums.count(x) != 2, nums))
+        
+        xor = 0
+        for i in nums:
+            xor ^= i
+        
+        xor &= -xor
+        a, b = 0, 0
+        for i in nums:
+            if xor & i:
+                a ^= i
+            else:
+                b ^= i
+        return a, b
