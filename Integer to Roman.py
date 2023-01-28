77d38e98df5485d3236c1289a98a1e1bb9c14058
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-01-28 22:12:30 +0800

    Create Integer to Roman.py

---
+++ Integer to Roman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        m = ["", "M", "MM", "MMM", "MMMM"]
+        c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+        thousands = m[num // 1000]
+        hundreds = c[(num % 1000) // 100]
+        tens = x[(num % 100) // 10]
+        ones = i[num % 10]
+        ans = (thousands + hundreds + tens + ones)
+        return ans
