37a31d3f676d3c6f51605052abd4e2ea6f8c6571
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-02-02 23:42:00 +0800

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return permutations(nums)
