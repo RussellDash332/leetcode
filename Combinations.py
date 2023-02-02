90cf94a70e6cb87e660bfb78e5fd732057453f4b
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-02-02 23:41:00 +0800

    Update Combinations.py

--- Combinations.py
+++ Combinations.py
@@ -6,5 +6,10 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        tup = lambda x: tuple(sorted(x))
-        return set(map(tup, permutations(range(1,n+1), k)))
+        s = set(range(1, n+1))
+        if k <= n//2:
+            tup = lambda x: tuple(sorted(x))
+            return set(map(tup, permutations(s, k)))
+        else:
+            tup = lambda x: tuple(sorted(s-set(x)))
+            return set(map(tup, permutations(s, n-k)))
