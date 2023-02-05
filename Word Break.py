2bd5c7749714d719bd79aecda19ad454bf409630
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-02-06 01:27:49 +0800

    Create Word Break.py

---
+++ Word Break.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [True] + [False]*len(s)
+        wordDict = set(wordDict)
+        wordDict.add('')
+        for i in range(len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
