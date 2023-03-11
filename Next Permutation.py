e0608563f5a8ec56603df7859eddad464f970fd7
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-03-11 21:19:58 +0800

    Create Next Permutation.py

---
+++ Next Permutation.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for k in range(len(nums)-2, -2, -1):
+            if k == -1 or nums[k] < nums[k+1]: break
+        if k == -1: return nums.reverse()
+        for l in range(len(nums)-1, k-1, -1):
+            if l == k or nums[l] > nums[k]: break
+        nums[k], nums[l] = nums[l], nums[k]
+        new = nums[:k+1] + nums[k+1:][::-1]
+        while nums: nums.pop()
+        nums.extend(new)
