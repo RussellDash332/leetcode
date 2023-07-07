4cec335a6ae4cd23117e88d122ba254337c571f2
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-07-07 14:34:47 +0800

    Create Richest Customer Wealth.py

---
+++ Richest Customer Wealth.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        return max(map(sum, accounts))
